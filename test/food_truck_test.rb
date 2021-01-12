1f885ec86d62cf1896ba310f220b0b9f03efbe43
Katy La Tour <klatour324@gmail.com>
2021-01-12 14:31:10 -0600

    Create test to check stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -7,26 +7,28 @@ class FoodTruckTest < Minitest::Test
 
   def test_it_exists_with_attributes
     food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
     assert_instance_of FoodTruck, food_truck
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_equal ({}), food_truck.inventory
+  end
+
+  def test_it_can_check_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+
+    assert_equal ({item1 => 30}), food_truck.inventory
   end
 end
 
 
 
 
-
-
-
-
-# pry(main)> food_truck.check_stock(item1)
-# #=> 0
-#
-# pry(main)> food_truck.stock(item1, 30)
-#
-# pry(main)> food_truck.inventory
 # #=> {#<Item:0x007f9c56740d48...> => 30}
 #
 # pry(main)> food_truck.check_stock(item1)
