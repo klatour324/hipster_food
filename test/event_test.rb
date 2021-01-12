63f97279d935f01b5b4128cbda61d517d4d6bc46
Katy La Tour <klatour324@gmail.com>
2021-01-12 15:25:39 -0600

    Create tests to add food truck and keep track of food truck names

--- test/event_test.rb
+++ test/event_test.rb
@@ -12,6 +12,30 @@ class EventTest < MiniTest::Test
     assert_equal "South Pearl Street Farmers Market", event.name
     assert_equal [], event.food_trucks
   end
+
+  def test_it_keeps_track_of_food_truck_names
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    assert_equal [food_truck1, food_truck2, food_truck3], event.food_trucks
+    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], event.food_truck_names
+  end
 end
 
 
@@ -24,48 +48,31 @@ end
 
 
 
-# pry(main)> food_truck1 = FoodTruck.new("Rocky Mountain Pies")
 # #=> #<FoodTruck:0x00007fe1348a1160...>
 #
-# pry(main)> item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 # #=> #<Item:0x007f9c56740d48...>
 #
-# pry(main)> item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 # #=> #<Item:0x007f9c565c0ce8...>
 #
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
 # #=> #<Item:0x007f9c562a5f18...>
 #
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
 # #=> #<Item:0x007f9c56343038...>
 #
-# pry(main)> food_truck1.stock(item1, 35)
 #
-# pry(main)> food_truck1.stock(item2, 7)
 #
-# pry(main)> food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
 # #=> #<FoodTruck:0x00007fe1349bed40...>
 #
-# pry(main)> food_truck2.stock(item4, 50)
 #
-# pry(main)> food_truck2.stock(item3, 25)
 #
-# pry(main)> food_truck3 = FoodTruck.new("Palisade Peach Shack")
 # #=> #<FoodTruck:0x00007fe134910650...>
 #
-# pry(main)> food_truck3.stock(item1, 65)
 #
-# pry(main)> event.add_food_truck(food_truck1)
 #
-# pry(main)> event.add_food_truck(food_truck2)
 #
-# pry(main)> event.add_food_truck(food_truck3)
 #
 # pry(main)> event.food_trucks
-# #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe1349bed40...>, #<FoodTruck:0x00007fe134910650...>]
 #
 # pry(main)> event.food_truck_names
-# #=> ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
 #
 # pry(main)> event.food_trucks_that_sell(item1)
 # #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe134910650...>]
