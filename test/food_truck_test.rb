5f8f8a2272b5357d2d4727a974ddb5831e9df4b5
Katy La Tour <klatour324@gmail.com>
2021-01-12 15:05:06 -0600

    Create tests to check stock and add stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -14,32 +14,35 @@ class FoodTruckTest < Minitest::Test
     assert_equal ({}), food_truck.inventory
   end
 
-  def test_it_can_check_stock
+  def test_it_has_no_stock_at_first
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
     assert_equal 0, food_truck.check_stock(item1)
+  end
+
+  def test_it_can_check_and_add_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
     food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
 
+
+
+    assert_equal 55, food_truck.check_stock(item1)
     assert_equal ({item1 => 30}), food_truck.inventory
+    assert_equal ({item1 => 55, item2 => 12}), food_truck.inventory
   end
 end
 
 
 
 
-# #=> {#<Item:0x007f9c56740d48...> => 30}
 #
-# pry(main)> food_truck.check_stock(item1)
-# #=> 30
 #
-# pry(main)> food_truck.stock(item1, 25)
-#
-# pry(main)> food_truck.check_stock(item1)
-# #=> 55
-#
-# pry(main)> food_truck.stock(item2, 12)
 #
 # pry(main)> food_truck.inventory
 # #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
